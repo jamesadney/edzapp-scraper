495caa5a8c98f965adfc0a62cf70bb6041738318
James Adney <jfadney@gmail.com>
2012-06-10 00:12:07 -0700

    disable DjangoJobPipeline by default

--- edzapp/settings.py
+++ edzapp/settings.py
@@ -14,7 +14,7 @@ SPIDER_MODULES = ['edzapp.spiders']
 NEWSPIDER_MODULE = 'edzapp.spiders'
 USER_AGENT = 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1'
 ITEM_PIPELINES = [
-    'edzapp.pipelines.DjangoJobPipeline',
+#    'edzapp.pipelines.DjangoJobPipeline',
 ]
 DOWNLOAD_DELAY = 3
 
