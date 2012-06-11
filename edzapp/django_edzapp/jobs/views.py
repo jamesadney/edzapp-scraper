cf163f0427cd56a377ae661d13cfb16dbfb944bf
James Adney <jfadney@gmail.com>
2012-06-11 13:59:52 -0700

    job table formatting

--- edzapp/django_edzapp/jobs/views.py
+++ edzapp/django_edzapp/jobs/views.py
@@ -4,19 +4,20 @@ from django.template import RequestContext
 from jobs.models import Job
 
 def index(request):
-	jobs = Job.objects.all()
-	return render_to_response('index.html',
-							  {"jobs" : jobs},
+    jobs = Job.objects.exclude(deadline=None).order_by("deadline")
+    saved_jobs = request.user.job_set.all()
+    return render_to_response('jobs/index.html',
+                              {"jobs" : jobs, "saved_jobs": saved_jobs},
                               context_instance=RequestContext(request))
 
 def all(request):
-	return render_to_response('jobs/all.html',
+    return render_to_response('jobs/all.html',
                               context_instance=RequestContext(request))
 
 def saved(request):
-	return render_to_response('jobs/saved.html',
+    return render_to_response('jobs/saved.html',
                               context_instance=RequestContext(request))
 
 def detail(request, job_id):
-	return render_to_response('jobs/detail.html',
+    return render_to_response('jobs/detail.html',
                               context_instance=RequestContext(request))
