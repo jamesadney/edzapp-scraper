0a8e5946171f2cc1e55c4b0c95177a78d3ab489c
James Adney <jfadney@gmail.com>
2012-06-12 04:19:55 -0700

    wip: star jobs

--- edzapp/django_edzapp/jobs/urls.py
+++ edzapp/django_edzapp/jobs/urls.py
@@ -5,4 +5,6 @@ urlpatterns = patterns('jobs.views',
     url(r'^all/$', 'all'),
     url(r'^saved/$', 'saved'),
     url(r'^(?P<job_id>\d+)/$', 'detail'),
+    url(r'^(?P<job_id>\d+)/star/$', 'star'),
+    url(r'^(?P<job_id>\d+)/unstar/$', 'unstar'),
 )
--- edzapp/django_edzapp/jobs/views.py
+++ edzapp/django_edzapp/jobs/views.py
@@ -1,26 +1,35 @@
-from django.shortcuts import render_to_response
-from django.template import RequestContext
+from django.shortcuts import render
 
 from jobs.models import Job
 
 def index(request):
     jobs = Job.objects.exclude(deadline=None).order_by("deadline")
     no_deadlines = Job.objects.exclude(date_posted=None).filter(deadline=None).order_by("date_posted")
-
-    saved_jobs = request.user.job_set.all()
-    return render_to_response('jobs/index.html',
-                              {"jobs" : jobs, 
-                               "no_deadlines" : no_deadlines},
-                              context_instance=RequestContext(request))
+    return render(request, 'jobs/index.html', {"jobs" : jobs, 
+                                                "no_deadlines" : no_deadlines})
 
 def all(request):
-    return render_to_response('jobs/all.html',
-                              context_instance=RequestContext(request))
+    return render(request, 'jobs/all.html')
 
 def saved(request):
-    return render_to_response('jobs/saved.html',
-                              context_instance=RequestContext(request))
+    return render(request, 'jobs/saved.html')
 
 def detail(request, job_id):
-    return render_to_response('jobs/detail.html',
-                              context_instance=RequestContext(request))
+    return render(request, 'jobs/detail.html')
+
+def star(request, job_id):
+    #TODO: fix csrf
+    if request.method == "POST":
+        job = Job.objects.get(job_id=job_id)
+        request.user.job_set.add(job)
+        return
+    else:
+        render(request, 'jobs/star.html')
+
+def unstar(request, job_id):
+    if request.method == "POST":
+        job = Job.objects.get(job_id=job_id)
+        request.user.job_set.remove(job)
+        return
+    else:
+        render(request, 'jobs/unstar.html')
